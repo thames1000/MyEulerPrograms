5e5b4b8f9e0af29c3a6ed701e8cd71cac8ca3747
James Lambert <james.lambert3995@gmail.com>
2022-04-21 12:32:37 -0400

    Added documentation and changed how the is_prime function returns the results

--- Python/euler003.py
+++ Python/euler003.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
 ##############################################################################
 # Program by: James Lambert                                                  #
-# Date Updated: 20220419                                                     #
+# Date Updated: 20220421                                                     #
 ##############################################################################
 # The prime factors of 13195 are 5, 7, 13 and 29.                            #
 #                                                                            #
 # What is the largest prime factor of the number 600851475143 ?              #
 ##############################################################################
 def is_prime(number):
-    # used to determine if a number is prime, returning boolean value,
-    # and the prime factor that number is divisible by
+    """
+    Takes one paramater 'number', determines if that number is prime.
+    If False, returns (int that 'number' is evenly divisible by, False).
+    If True, returns (0, True).
+
+    returns tuple(int, boolean)
+    """
+    result = (0, True)
     for num_check in range(2, int(number ** 0.5) + 1):
         if number % num_check == 0:
-            return num_check, False
-    return "None", True
+            result = (num_check, False)
+            break
+    return result
 
 
 def factor(big_number):
-    # create a list of prime factors of big_number
+    """
+    Takes one paramater 'big_number', and returns a list of the prime
+    factors of that number
+
+    returns [int,**]
+    """
     factor_list = []
     temp_num = big_number               # number we can change
     temp = is_prime(temp_num)           # tuple results from is_prime
@@ -30,5 +42,5 @@ def factor(big_number):
 
 
 big_factors = factor(600851475143)
-print(big_factors)
-print(max(big_factors))
+print(big_factors)       # List of all factors
+print(max(big_factors))  # Grabs largest value from list of factors
